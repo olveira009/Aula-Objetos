99b7683d26d69651f90ebfb586efc69a9584f902
olveira009 <douglasoliveira20099@gmail.com>
2024-06-11 16:11:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,118 @@
-let a = true
-let b = false 
-let c = true
+Atividade 1 
 
-console.log(a&&b)
-console.log(b&&c)
-console.log(a&&c)
-console.log(a&&b&&c)
+const filme = {
+	nome: "Auto da Compadecida", 
+	ano: 2000, 
+	elenco: [
+		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+		"Virginia Cavendish"
+		], 
+	transmissoesHoje: [
+		{canal: "Telecine", horario: "21h"}, 
+		{canal: "Canal Brasil", horario: "19h"}, 
+		{canal: "Globo", horario: "14h"}
+		]
+}
 
-console.log(a||b)
-console.log(b||c)
-console.log(a||c)
-console.log(a||b||c)
+console.log(filme.elenco[0])
+console.log(filme.elenco[filme.elenco.length - 1])
+console.log(filme.transmissoesHoje[2])
 
-let nome;
-let idade;
+// Matheus Nachtergaele
+// Virginia Cavendish
 
-// Perguntar ao usuário seu nome
-nome = prompt("Qual é o seu nome?");
+Atividade 2 
 
-// Perguntar ao usuário sua idade e converter para número
-idade = prompt("Qual é a sua idade?");
+function minhaFuncao(objeto, propriedade) {
+	return objeto[propriedade]
+}
 
-// Imprimir a mensagem na tela
-console.log("Olá " + nome + ", você tem " + idade + " anos.");
+const pessoa = {
+  nome: "Caio", 
+  idade: 23, 
+  backender: false
+}
+
+console.log(minhaFuncao(pessoa, "backender"))
+console.log(minhaFuncao(pessoa, "altura"))
+
+// false
+// undefined
+
+Atividade 3 
+
+// a = 
+const pessoa = {
+    nome: "Vitor Hugo",
+    apelidos: ["Vitinho", "Vitão", "Vit"]
+}
+function imprimirMensagem(objeto) {
+    const { nome, apelidos } = objeto;
+    const mensagem = `Eu sou ${nome}, mas pode me chamar de: ${apelidos.join(", ")}`;
+    console.log(mensagem);
+}
+imprimirMensagem(pessoa);
+
+
+// b = 
+const novaPessoa = { ...pessoa, apelidos: ["Huguinho", "Hugo", "VH"] };
+imprimirMensagem(novaPessoa);
+
+Atividade 3 
+
+
+// Criando dois objetos diferentes
+const pessoa1 = {
+    nome: "Alice",
+    idade: 30,
+    profissao: "Engenheira"
+};
+
+const pessoa2 = {
+    nome: "Bob",
+    idade: 25,
+    profissao: "Professor"
+};
+
+
+// Escrevendo a função
+function getInfo(objeto) {
+    const { nome, idade, profissao } = objeto;
+    return [
+        nome,
+        nome.length,
+        idade,
+        profissao,
+        profissao.length
+    ];
+}
+
+
+Atividade 4
+
+let carrinho = [];
+
+const fruta1 = {
+    nome: "Maçã",
+    disponibilidade: true
+};
+
+const fruta2 = {
+    nome: "Banana",
+    disponibilidade: true
+};
+
+const fruta3 = {
+    nome: "Laranja",
+    disponibilidade: true
+};
+
+function adicionarAoCarrinho(fruta) {
+    carrinho.push(fruta);
+}
+adicionarAoCarrinho(fruta1);
+adicionarAoCarrinho(fruta2);
+adicionarAoCarrinho(fruta3);
+
+console.log(carrinho);
+
